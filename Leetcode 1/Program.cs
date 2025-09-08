3d1304c4563c2fea2bed05e084c1abc407917199
ShalomShrek <callum.smyth03@gmail.com>
2025-09-08 17:46:13 +0200

    Add project files.

---
+++ Leetcode 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Leetcode_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] result = Solution.GetNoZeroIntegers(n);
+            Console.WriteLine("[" + result[0] + "," + result[1] + "]");
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Leetcode 1/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_1
+{
+    internal class Solution
+    {
+        public static int[] GetNoZeroIntegers(int n)
+        {
+            int[] result = new int[2];
+            Boolean found = false;
+            int a = 0;
+            int b = 0;
+            while (found == false) 
+            {
+                a++;
+                b = n - a;
+                if ((!(a.ToString().Contains("0"))) && (!(b.ToString().Contains("0"))))
+                {
+                    result[0] = a;
+                    result[1] = b;
+                    found = true;
+                }
+            }
+            return result;
+        }
+    }
+}
